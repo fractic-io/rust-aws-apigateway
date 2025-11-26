6165d6a0f4cdc652788468ad94f9b2460b3b98d1
Mart van Buren <mart@fractic.io>
2025-11-26 18:57:21 +0900

    [w/function] Absolute import paths.

--- src/handle_with_function/macros.rs
+++ src/handle_with_function/macros.rs
@@ -4,19 +4,19 @@ macro_rules! aws_lambda_handle_with_function {
         async fn __handler(
             event: lambda_runtime::LambdaEvent<aws_lambda_events::apigw::ApiGatewayProxyRequest>,
         ) -> Result<aws_lambda_events::apigw::ApiGatewayProxyResponse, lambda_runtime::Error> {
-            let metadata = match parse_request_metadata(&event.payload) {
+            let metadata = match $crate::parse_request_metadata(&event.payload) {
                 Ok(m) => m,
-                Err(e) => return build_error(e),
+                Err(e) => return $crate::build_error(e),
             };
-            match parse_request_data::<$request_data_type>(&event.payload) {
+            match $crate::parse_request_data::<$request_data_type>(&event.payload) {
                 Ok(obj) => match $validator(&obj, metadata) {
                     Ok(_) => match $func(obj).await {
-                        Ok(result) => build_result(result),
-                        Err(func_error) => build_error(func_error),
+                        Ok(result) => $crate::build_result(result),
+                        Err(func_error) => $crate::build_error(func_error),
                     },
-                    Err(validation_error) => build_error(validation_error),
+                    Err(validation_error) => $crate::build_error(validation_error),
                 },
-                Err(request_parsing_error) => build_error(request_parsing_error),
+                Err(request_parsing_error) => $crate::build_error(request_parsing_error),
             }
         }
         $crate::aws_lambda_handle_raw!(__handler);
@@ -25,16 +25,16 @@ macro_rules! aws_lambda_handle_with_function {
         async fn __handler(
             event: lambda_runtime::LambdaEvent<aws_lambda_events::apigw::ApiGatewayProxyRequest>,
         ) -> Result<aws_lambda_events::apigw::ApiGatewayProxyResponse, lambda_runtime::Error> {
-            let metadata = match parse_request_metadata(&event.payload) {
+            let metadata = match $crate::parse_request_metadata(&event.payload) {
                 Ok(m) => m,
-                Err(e) => return build_error(e),
+                Err(e) => return $crate::build_error(e),
             };
             match $validator(metadata) {
                 Ok(_) => match $func().await {
-                    Ok(result) => build_result(result),
-                    Err(func_error) => build_error(func_error),
+                    Ok(result) => $crate::build_result(result),
+                    Err(func_error) => $crate::build_error(func_error),
                 },
-                Err(validation_error) => build_error(validation_error),
+                Err(validation_error) => $crate::build_error(validation_error),
             }
         }
         $crate::aws_lambda_handle_raw!(__handler);
@@ -44,8 +44,8 @@ macro_rules! aws_lambda_handle_with_function {
             event: lambda_runtime::LambdaEvent<ApiGatewayProxyRequest>,
         ) -> Result<ApiGatewayProxyResponse, lambda_runtime::Error> {
             match $func(event.payload.headers, event.payload.body).await {
-                Ok(result) => Ok(build_simple(result)),
-                Err(func_error) => build_error(func_error),
+                Ok(result) => Ok($crate::build_simple(result)),
+                Err(func_error) => $crate::build_error(func_error),
             }
         }
         $crate::aws_lambda_handle_raw!(__handler);
