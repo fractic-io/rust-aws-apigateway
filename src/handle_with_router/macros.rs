9f65345ac13ab608a2f2b7dde20129f001fde981
Mart van Buren <mart@fractic.io>
2025-11-27 12:46:27 +0900

    Place config init behind std::sync::OnceLock.

--- src/handle_with_router/macros.rs
+++ src/handle_with_router/macros.rs
@@ -6,8 +6,11 @@ macro_rules! aws_lambda_handle_with_router {
                 ::aws_lambda_events::apigw::ApiGatewayProxyRequest,
             >,
         ) -> Result<::aws_lambda_events::apigw::ApiGatewayProxyResponse, ::lambda_runtime::Error> {
-            let config = $config;
-            config.handle(event).await
+            static CONFIG: ::std::sync::OnceLock<
+                $crate::handle_with_router::routing_config::RoutingConfig,
+            > = ::std::sync::OnceLock::new();
+            let config_ref = CONFIG.get_or_init(|| $config);
+            config_ref.handle(event).await
         }
         $crate::aws_lambda_handle_raw!(__handler);
     };
